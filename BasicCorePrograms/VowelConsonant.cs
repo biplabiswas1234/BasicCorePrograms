7d359430bed106edf7c53be0c446adaecfba9089
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-05-13 14:19:33 +0530

    UC9[Biplab] Add: Check vowel and consonant

---
+++ BasicCorePrograms/VowelConsonant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    internal class VowelConsonant
+    {
+        public void VowelCon()
+        {
+
+            Console.WriteLine("Enter an alphabet");
+
+            char check = Convert.ToChar(Console.ReadLine());
+            if (check == 'a' || check == 'e' || check == 'i' || check == 'o' || check == 'u' || check == 'A' || check == 'E' || check == 'I' || check == 'O' || check == 'U')
+            {
+                Console.WriteLine("The character {0} is vowel", check);
+            }
+            else
+            {
+                Console.WriteLine("The character {0} is consonent", check);
+            }
+        }
+    }
+}
